83ee10d34c06007dbda287eaae1d3a3627990a6d
Derek Skelton <per76632@adobe.com>
2020-08-01 14:56:56 -0500

    First push
    Adding header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './footer.css'
+
+const Footer=()=> {
+  return (
+    <div className="Footer">
+          <div>Example footer</div>
+    </div>
+  )
+}
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './header.css'
+const Header=()=> {
+  return (
+    <div className="Header">
+          <div>Home</div>
+    </div>
+  )
+}
+export default Header;
